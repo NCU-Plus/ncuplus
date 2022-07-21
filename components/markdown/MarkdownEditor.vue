eb9cfe91d99b00611cefa29fe99759f52499840d
ppodds <oscar20020629@gmail.com>
2022-07-22 02:02:19 +0800

    Feat: add MarkdownEditor

---
+++ components/markdown/MarkdownEditor.vue
@@ -0,0 +1,71 @@
+<template>
+  <textarea ref="editor"></textarea>
+</template>
+
+<script setup lang="ts">
+import DOMPurify from "isomorphic-dompurify";
+import { marked } from "marked";
+import hljs from "highlight.js";
+
+const props = defineProps<{
+  initialValue?: string;
+  placeholder?: string;
+}>();
+const editor = ref<HTMLTextAreaElement | null>(null);
+// ref EasyMDE instance would cause serious problems
+let mdeInstance: EasyMDE | null = null;
+marked.setOptions({
+  highlight: (code, lang) => {
+    const language = hljs.getLanguage(lang) ? lang : "plaintext";
+    return hljs.highlight(code, { language }).value;
+  },
+  langPrefix: "hljs language-",
+  breaks: true,
+});
+
+watchEffect(async () => {
+  if (editor.value) {
+    const EasyMDE = (await import("easymde")).default;
+    mdeInstance = new EasyMDE({
+      element: editor.value,
+      placeholder: props.placeholder,
+      initialValue: props.initialValue,
+      previewRender: (plainText) =>
+        DOMPurify.sanitize(
+          `<article class="prose text-left overflow-hidden">${marked.parse(
+            plainText,
+          )}</article>`,
+        ),
+      sideBySideFullscreen: false,
+      toolbar: [
+        "bold",
+        "italic",
+        "strikethrough",
+        "heading",
+        "code",
+        "quote",
+        "unordered-list",
+        "ordered-list",
+        "link",
+        "image",
+        "table",
+        "horizontal-rule",
+        "preview",
+        "side-by-side",
+        "guide",
+      ],
+      spellChecker: false,
+      nativeSpellcheck: false,
+    });
+  }
+});
+
+function getMdeInstance() {
+  if (!mdeInstance) throw new Error("editor is not ready");
+  return mdeInstance;
+}
+
+defineExpose({
+  getMdeInstance,
+});
+</script>
