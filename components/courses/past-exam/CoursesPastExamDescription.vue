4af48e8121ef97bf8b83ecfe55b4a2ec87bf0bcd
ppodds <oscar20020629@gmail.com>
2022-07-24 21:46:15 +0800

    Feat: update past exam description

--- components/courses/past-exam/CoursesPastExamDescription.vue
+++ components/courses/past-exam/CoursesPastExamDescription.vue
@@ -6,7 +6,7 @@
     <div class="p-2 bg-white rounded-b-md">
       <p>本區只開放上傳考古題及作業，若上傳檔案不符或是被檢舉將會被移除。</p>
       <p>檔案請根據年度寫上對應敘述，方便同學查找。</p>
-      <p>支援檔案格式: doc(x)/ppt(x)/pdf/jp(e)g/png/zip/7z/rar</p>
+      <p>支援檔案格式: docx/pptx/pdf/jp(e)g/png/zip/7z/rar</p>
       <p>每個檔案的大小限制為15MB</p>
       <br />
       <p>請先登入後才能上傳檔案</p>
