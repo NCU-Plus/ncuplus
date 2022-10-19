03011265bfa6dd74ad73a6a8496af723b286a05e
ppodds <oscar20020629@gmail.com>
2022-10-19 15:17:24 +0800

    chore: setup cypress and unit test ci
    fix: ci failed
    
    fix: husky install cause cypress install failed
    
    fix: start server failed in ci
    
    fix: cypress not installed
    
    fix: husky install cause cypress test failed
    
    fix: cypress can't find tsconfig
    
    fix: try to fix tsconfig issue again
    
    fix: download artifact failed
    
    fix: split .output and tsconfig to solve issue
    
    fix: not extend generated tsconfig to solve issue

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "d188rn",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
