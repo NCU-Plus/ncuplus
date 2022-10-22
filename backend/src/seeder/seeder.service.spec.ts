7759da7901c3f0d2a820b29942418c8138db893a
ppodds <oscar20020629@gmail.com>
2022-10-23 03:34:57 +0800

    fix: seederService test

--- backend/src/seeder/seeder.service.spec.ts
+++ backend/src/seeder/seeder.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CollegeSeederService } from './college-seeder/college-seeder.service';
 import { SeederService } from './seeder.service';
 
 describe('SeederService', () => {
@@ -6,7 +7,13 @@ describe('SeederService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SeederService],
+      providers: [
+        SeederService,
+        {
+          provide: CollegeSeederService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<SeederService>(SeederService);
