08c883a7a9b45a262cc075ecf9f10731397c6069
wama <36957874+april1026@users.noreply.github.com>
2022-10-22 19:45:41 +0800

    feat: Change baseUrl port to 4000

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://localhost:3001",
+    baseUrl: "http://localhost:4000",
   },
 });
