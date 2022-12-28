6047438a28811d3d818b5f85376d0693915f9571
ppodds <oscar20020629@gmail.com>
2022-12-29 01:10:09 +0800

    feat: sitemap generation

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('sitemap.txt')
+  @Header('Content-Type', 'text/plain; charset=utf-8')
+  async sitemap(): Promise<string> {
+    return await this.appService.generateSitemap();
   }
 }
