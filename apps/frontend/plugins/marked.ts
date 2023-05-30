cb857e52b413fb4a69cd3b0d695a6cd7914ba931
ppodds <oscar20020629@gmail.com>
2023-05-30 22:23:51 +0800

    fix(frontend): marked deprecated warning

--- apps/frontend/plugins/marked.ts
+++ apps/frontend/plugins/marked.ts
@@ -1,14 +1,27 @@
 import { marked } from "marked";
 import hljs from "highlight.js";
+import { markedHighlight } from "marked-highlight";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { mangle } from "marked-mangle";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { gfmHeadingId } from "marked-gfm-heading-id";
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.hook("app:created", () => {
+    marked.use(
+      markedHighlight({
+        langPrefix: "hljs language-",
+        highlight: (code, lang) => {
+          const language = hljs.getLanguage(lang) ? lang : "plaintext";
+          return hljs.highlight(code, { language }).value;
+        },
+      }),
+      mangle(),
+      gfmHeadingId(),
+    );
     marked.setOptions({
-      highlight: (code, lang) => {
-        const language = hljs.getLanguage(lang) ? lang : "plaintext";
-        return hljs.highlight(code, { language }).value;
-      },
-      langPrefix: "hljs language-",
       breaks: true,
     });
   });
